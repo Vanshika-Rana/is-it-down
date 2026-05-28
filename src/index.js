dcd86429dcbbe75fe52528fe4990f73771711736
Vanshika-Rana <ranavanshika172000@gmail.com>
2026-05-28 15:43:33 +0530

    fix: deploy node_modules and remove dotenv dependency

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
-require("dotenv").config();
-
 const { initDb, closeDb } = require("./db");
 const { closeCache } = require("./cache");
 const { startServer, stopServer } = require("./server");
 const { startWorker, stopWorker } = require("./worker");
 
 async function bootstrap() {
-  await initDb();
-  startServer();
-  startWorker();
+	await initDb();
+	startServer();
+	startWorker();
 }
 
 async function shutdown(signal) {
-  console.log(`Received ${signal}, shutting down...`);
-  stopWorker();
-  stopServer();
-  await closeCache();
-  await closeDb();
-  process.exit(0);
+	console.log(`Received ${signal}, shutting down...`);
+	stopWorker();
+	stopServer();
+	await closeCache();
+	await closeDb();
+	process.exit(0);
 }
 
 process.on("SIGINT", () => shutdown("SIGINT"));
 process.on("SIGTERM", () => shutdown("SIGTERM"));
 
 bootstrap().catch((error) => {
-  console.error("Failed to start application:", error);
-  process.exit(1);
+	console.error("Failed to start application:", error);
+	process.exit(1);
 });
